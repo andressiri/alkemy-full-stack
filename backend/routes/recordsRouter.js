1a741cbf0e832cd5bbe27b893472d1d1685ef7cf
Andrés Siri <andres.siri@hotmail.com>
2022-04-22 17:08:41 -0300

    recordsRouter - fix mistake commiting file without saving

--- backend/routes/recordsRouter.js
+++ backend/routes/recordsRouter.js
@@ -3,14 +3,14 @@ const recordsRouter = express.Router();
 
 recordsRouter.route('/')
   // get records handle
-  .get('/', require('../controllers/records/getRecords.js'))
+  .get(require('../controllers/records/getRecords.js'))
   // save record handle
   .post(require('../controllers/records/saveRecord.js'));
 
-recordsRouter.route('/')
+recordsRouter.route('/:id')
   // update record handle
-  .put('/:id', require('../controllers/records/updateRecord.js'))
+  .put(require('../controllers/records/updateRecord.js'))
   // delete record handle
-  .delete('/:id', require('../controllers/records/deleteRecord.js'));
+  .delete(require('../controllers/records/deleteRecord.js'));
 
 module.exports = recordsRouter;
