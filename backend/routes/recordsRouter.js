0f8cd17bc0e43b3822eea88c442daf844f106d06
Andrés Siri <andres.siri@hotmail.com>
2022-04-22 16:56:43 -0300

    change syntax at records router for routes that share url but have different method

--- backend/routes/recordsRouter.js
+++ backend/routes/recordsRouter.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const recordsRouter = express.Router();
 
-// get records handle
-recordsRouter.get('/', require('../controllers/records/getRecords.js'));
+recordsRouter.route('/')
+  // get records handle
+  .get('/', require('../controllers/records/getRecords.js'))
+  // save record handle
+  .post(require('../controllers/records/saveRecord.js'));
 
-// save record handle
-recordsRouter.post('/', require('../controllers/records/saveRecord.js'));
-
-// update record handle
-recordsRouter.put('/:id', require('../controllers/records/updateRecord.js'));
-
-// delete record handle
-recordsRouter.delete('/:id', require('../controllers/records/deleteRecord.js'));
+recordsRouter.route('/')
+  // update record handle
+  .put('/:id', require('../controllers/records/updateRecord.js'))
+  // delete record handle
+  .delete('/:id', require('../controllers/records/deleteRecord.js'));
 
 module.exports = recordsRouter;
