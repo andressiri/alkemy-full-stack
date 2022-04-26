07e25fcd65781a47e47d98b68f44ac1a336d3ddb
Andrés Siri <andres.siri@hotmail.com>
2022-04-26 19:13:45 -0300

    improve error handling middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,13 +1,16 @@
 // overwrite express default error handle
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-
   res.status(statusCode);
-
-  res.json({
+  
+  const json = res.json.message ? res.json : {
     message: err.message,
     stack: process.env.NODE_ENV !== 'development' ? null : err.stack
-  });
+  };
+  
+  res.json(json);
+
+  if (process.env.NODE_ENV === 'development') console.log(json.message);
 }
 
 module.exports = {
