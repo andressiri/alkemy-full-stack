56a9147244d4614e7ae45db461b30bac9dda390c
Andrés Siri <andres.siri@hotmail.com>
2022-04-27 01:17:07 -0300

    implement get records controller

--- backend/controllers/records/getRecords.js
+++ backend/controllers/records/getRecords.js
@@ -2,7 +2,17 @@
 // @route  GET /api/v1/records
 // @access  Private
 const asyncHandler = require('express-async-handler');
+const Record = require('../../models/Record.js');
 
 module.exports = asyncHandler(async (req, res) => {
-  res.json({message: 'Get records'});
+  let message = 'Records obtained';
+
+  const records = await Record.findAll({
+    raw: true,
+    where: {user_uuid: req.user.user_uuid}
+  });
+
+  if (!records[0]) message = `No records saved by ${req.user.name}`;
+  
+  res.status(200).json({message, records});
 })
