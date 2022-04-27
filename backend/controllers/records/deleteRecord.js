36e1cfaa68b60937e2a277ce57f56566e9902831
Andrés Siri <andres.siri@hotmail.com>
2022-04-27 13:40:15 -0300

    implement delete record controller

--- backend/controllers/records/deleteRecord.js
+++ backend/controllers/records/deleteRecord.js
@@ -2,7 +2,31 @@
 // @route  DELETE /api/v1/records/:uuid
 // @access  Private
 const asyncHandler = require('express-async-handler');
+const Record = require('../../models/Record.js');
 
 module.exports = asyncHandler(async (req, res) => {
-  res.json({message: `Deleted record ${req.params.uuid}`});
+  const record_uuid = req.params.uuid;
+
+  const record = await Record.findOne({
+    raw: true,
+    where: {record_uuid: record_uuid}
+  });
+
+  if (!record) {
+    res.status(404);
+    throw new Error('That record does not exist');
+  };
+
+  if (record.user_uuid !== req.user.user_uuid) {
+    res.status(403);
+    throw new Error('You can\'t delete that resource');
+  };
+
+  const recordDelete = await Record.destroy({
+    where: {record_uuid: record_uuid}
+  });
+
+  console.log(recordDelete);
+
+  res.status(200).json({message: `Deleted record ${req.params.uuid}`});
 })
