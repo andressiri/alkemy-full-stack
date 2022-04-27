879d0359c3bb544e86b3248aace33496fc1c6d84
Andrés Siri <andres.siri@hotmail.com>
2022-04-26 23:34:19 -0300

    improve routes protect middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,31 +9,32 @@ const protectRoute = asyncHandler(async (req, res, next) => {
   console.log(req.headers);
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-          // Get token from header
-      token = req.headers.authorization.split(' ')[1];
 
-      console.log(token);
+    // Get token from header
+    token = req.headers.authorization.split(' ')[1];
 
-      // Verify token
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    // Verify token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Get user from the token
-      req.user = await User.findOne({ 
-        attributes: {exclude: ['password']},
-        where: {user_uuid: decodedToken.user_uuid} 
-      });
+    // Get user from the token
+    req.user = await User.findOne({
+      raw: true, 
+      attributes: {exclude: ['password']},
+      where: {user_uuid: decodedToken.user_uuid} 
+    });
 
-      next();
-    } catch (err) {
+    // Check if it was a wrong token or something went wrong
+    if (!req.user) {
       res.status(401);
       throw new Error('Not authorized');
     };
+
+    next();
   };
 
   if (!token) {
     res.status(401);
-    throw new Error('Not authorized');
+    throw new Error('Not authorized, no token');
   };
 })
 
