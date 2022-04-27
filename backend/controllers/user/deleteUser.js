19203296e343336d18362b5e8edca3751575a1c1
Andrés Siri <andres.siri@hotmail.com>
2022-04-27 14:14:42 -0300

    implement delete user controller

--- backend/controllers/user/deleteUser.js
+++ backend/controllers/user/deleteUser.js
@@ -2,7 +2,19 @@
 // @route  DELETE /api/v1/user/delete/:uuid
 // @access  Private
 const asyncHandler = require('express-async-handler');
+const User = require('../../models/User.js');
 
 module.exports = asyncHandler(async (req, res) => {
-  res.json({message: 'Delete account'});
+  const user_uuid = req.params.uuid;
+
+  if (user_uuid !== req.user.user_uuid) {
+    res.status(403);
+    throw new Error('You can\'t delete that resource');
+  };
+
+  await User.destroy({
+    where: {user_uuid: user_uuid}
+  });
+
+  res.status(200).json({message: `Deleted user ${user_uuid}`});
 })
