b90f04a3c4801fda8caaeea975d42c251f014b44
Andrés Siri <andres.siri@hotmail.com>
2022-04-27 19:11:24 -0300

    implement change password controller

--- backend/controllers/user/changePassword.js
+++ backend/controllers/user/changePassword.js
@@ -2,7 +2,27 @@
 // @route  PUT /api/v1/user/password
 // @access  Private
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const hashPassword = require('../../functions/hashPassword.js');
+const User = require('../../models/User.js');
 
 module.exports = asyncHandler(async (req, res) => {
-  res.json({message: 'Change password'});
+  const password = req.body.password;
+
+  if (!password) {
+    res.status(400);
+    throw new Error('Please send a new password');
+  };
+
+  const hashedPassword = await hashPassword(password);
+
+  await User.update({
+      password: hashedPassword
+    },
+    {
+      where: {user_uuid: req.user.user_uuid}
+    }
+  );
+
+  res.status(200).json({message: 'Password updated'});
 })
