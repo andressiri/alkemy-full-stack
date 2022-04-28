f457389e13f40d9b40d2b24c155e297acc76eaa6
Andrés Siri <andres.siri@hotmail.com>
2022-04-27 21:29:40 -0300

    install material ui and roboto fountsource

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,10 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
