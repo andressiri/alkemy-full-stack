7677147cdaf1b86982a758a75d1da567d493b886
Andrés Siri <andres.siri@hotmail.com>
2022-04-30 22:44:20 -0300

    make autcomplete reusable

--- frontend/src/components/FilterBarAutocomplete.jsx
+++ frontend/src/components/FilterBarAutocomplete.jsx
@@ -1,11 +1,17 @@
 import React, {useState} from 'react';
+import styled from '@emotion/styled';
 import TextField from '@mui/material/TextField';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
-import styled from '@emotion/styled';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function ConceptAutocomplete() {
+function CategoryAutocomplete({parentToChild}) {
   const [value, setValue] = useState(null);
   const filter = createFilterOptions();
+  const {specifics} = parentToChild;
 
   const StyledAutocomplete = styled(Autocomplete)({
     "& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
@@ -29,7 +35,7 @@ function ConceptAutocomplete() {
     "& .MuiAutocomplete-inputRoot": {
       color: "#9c27b0",
       // This matches the specificity of the default styles at https://github.com/mui-org/material-ui/blob/v4.11.3/packages/material-ui-lab/src/Autocomplete/Autocomplete.js#L90
-      '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-child': {
+      '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-of-type': {
         // Default left padding is 6px
         paddingTop: '3px',
         paddingLeft: '5px',
@@ -63,6 +69,21 @@ function ConceptAutocomplete() {
     {content: 'Taxes'},
     {content: 'Travel expenses'},
   ];
+
+  const categories = [
+    {content: 'Food'},
+    {content: 'Gym'},
+    {content: 'Office'},
+    {content: 'Personal'},
+    {content: 'Presents'},
+  ];
+
+  const setOptions = () => {
+    switch (specifics) {
+      case 'Concept': return concepts;
+      case 'Category': return categories;
+    };
+  };
 
   const onAutcompleteChange = (event, newValue) => {
     if (typeof newValue === 'string') {
@@ -107,28 +128,51 @@ function ConceptAutocomplete() {
     // Regular option
     return option.content;
   };
+
+  const autocompleteRenderOption = (props, option) => (
+    <ListItem {...props} key={option.content}>
+      <ListItemText primary={option.content} />
+      <ListItemSecondaryAction>
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          //onClick={(e) => handleOptionDelete(option)} TODO
+        >
+          <DeleteIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
   
   
   return (
     <StyledAutocomplete
       color="secondary"
-      sx={{ width: 150, m: 1, color: '#ba68c8'}}
+      sx={{ width: 200, m: 1, color: '#ba68c8'}}
       value={value}
       onChange={onAutcompleteChange}
       filterOptions={autocompleteFilterOptions}
       selectOnFocus
       clearOnBlur
       handleHomeEndKeys
-      id="concept"
-      options={concepts}
+      id={specifics}
+      options={setOptions()}
       getOptionLabel={autocompleteGetOptionsLabel}
-      renderOption={(props, option) => <li {...props}>{option.content}</li>}
+      isOptionEqualToValue={(option, value) => option.id === value.id}
+      renderOption={autocompleteRenderOption}
       renderInput={(params) => (
-      <TextField {...params} label="Concept" color="secondary" size='small' sx={{borderColor: '#ba68c8', padding: 0}}/>
+      <TextField {...params} 
+        label={specifics}
+        color="secondary"
+        size='small'
+        sx={{
+          borderColor: '#ba68c8',
+          padding: 0
+        }}
+      />
       )}
     />
   );
 };
 
-export default ConceptAutocomplete;
-
+export default CategoryAutocomplete;
