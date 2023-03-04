5fde5e2be935be796c5a1a531c9ebb4992520dab
Andrés Siri <andres.siri@hotmail.com>
2023-03-04 13:42:57 -0300

    Serve front from public folder at backend

--- backend/index.js
+++ backend/index.js
@@ -35,9 +35,9 @@ app.use('/api/v1', require('./routes/router.js'));
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, './public')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 };
 
 app.use(errorHandler);
